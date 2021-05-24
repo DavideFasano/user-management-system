9f5db370402a832b8d14294e9c823c496eadcb94
DavideFasano <davide95fasano@gmail.com>
2021-05-24 13:03:32 +0200

    inizio lavoro su form di interesse

--- add_interesse.php
+++ add_interesse.php
@@ -1,12 +1,21 @@
 <?php
+
+use sarassoroberto\usm\entity\Interessi;
+
 require "./__autoload.php";
 $action = './add_interesse.php';
 $submit = 'aggiungi nuovo interesse';
 
 if($_SERVER['REQUEST_METHOD']==='GET'){
 
+    $nomeInteresse = '';
+    $interesseMessage = '';
+
 }
 
 if($_SERVER['REQUEST_METHOD']==='POST'){
+
+    $interesse = new Interessi($_POST['interesse']);
+    
     
 }
